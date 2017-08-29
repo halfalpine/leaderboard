9457394c20eb8d9b38da3524d2dbf44eab83e2a6
Dirk Peters <rocksandwoks@gmail.com>
2017-08-29 16:06:36 -0400

    Adapt webfonts to react env

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Leaderboard from './App';
 import registerServiceWorker from './registerServiceWorker';
+import WebFont from 'webfontloader';
+
+WebFont.load({
+  google: {
+    families: ['Roboto:100,400', 'saxmono', 'sans-serif']
+  },
+  typekit: {
+    id: 'jzo0zgz'
+  }
+});
 
 ReactDOM.render(<Leaderboard />, document.getElementById('root'));
 registerServiceWorker();
